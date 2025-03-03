b14c29462e9e58d7f3dcfb0b8212d0d2433e8355
Игорь <igrik82@gmail.com>
2025-03-03 17:09:31 +0500

    Note for keyword

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -7,3 +7,14 @@ return {
 		-- refer to the configuration section below
 	},
 }
+
+-- alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+--       -- signs = false, -- configure signs for some keywords individually
+--     },
+--TODO
+--   HACK = { icon = " ", color = "warning" },
+--   WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+--   PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+--   NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+--   TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+-- },
