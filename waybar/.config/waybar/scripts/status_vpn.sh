4706cb4ac30e72140032ab82303e6d228f4382ef
Игорь <igrik82@gmail.com>
2025-03-08 18:25:10 +0500

    Small changes

--- waybar/.config/waybar/scripts/status_vpn.sh
+++ waybar/.config/waybar/scripts/status_vpn.sh
@@ -5,21 +5,21 @@ DISABLED=""
 
 #echo $ENABLED
 snore() {
-  local IFS
-  [[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
-  read -r ${1:+-t "$1"} -u "$_snore_fd" || :
+    local IFS
+    [[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
+    read -r ${1:+-t "$1"} -u "$_snore_fd" || :
 }
 DELAY=1
 
 while snore $DELAY; do
-  # Disable NetworkManager
-  # if [[ $(nmcli -f GENERAL.STATE con show VPN | awk '{print $2}') == "activated" ]]; then
+    # Disable NetworkManager
+    # if [[ $(nmcli -f GENERAL.STATE con show VPN | awk '{print $2}') == "activated" ]]; then
 
-  if [[ $(ip -brief addr show wg0 | awk '{print $2}') == "UNKNOWN" ]]; then
-    echo $ENABLED
-  else
-    echo $DISABLED
-  fi
+    if [[ $(ip -brief addr show wg0 2>/dev/null | awk '{print $2}') == "UNKNOWN" ]]; then
+        echo $ENABLED
+    else
+        echo $DISABLED
+    fi
 done
 
 exit 0
