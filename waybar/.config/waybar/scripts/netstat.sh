3938c6c408d99bbef580d82f3385c2b7033a4808
Игорь <igrik82@gmail.com>
2025-08-31 16:04:15 +0500

    Changed theme to Monokai Pro

--- waybar/.config/waybar/scripts/netstat.sh
+++ waybar/.config/waybar/scripts/netstat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #vnstati --begin 2025-06-14 -d -L -vs -o - | swappy -f -
-vnstati --begin 2025-06-14 -d -L -o - | swappy -f -
+vnstati --begin 2025-08-14 -d -L -o - | swappy -f -
