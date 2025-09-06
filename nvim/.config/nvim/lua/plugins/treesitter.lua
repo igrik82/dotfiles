566d4e1a13218d87f0cea95239ac912e75df05f6
Игорь <igrik82@gmail.com>
2025-09-06 18:31:25 +0500

    Autotag plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,34 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	-- event = "VeryLazy",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    -- event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
 
-		-- configure telescope
-		treesitter.setup({
-			ensure_installed = {
-				"markdown",
-				"markdown_inline",
-				"python",
-				"c",
-				"cpp",
-				"bash",
-				"json",
-				"dockerfile",
-				"gitignore",
-				"lua",
-				"yaml",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+        -- configure telescope
+        treesitter.setup({
+            ensure_installed = {
+                "html",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "c",
+                "cpp",
+                "bash",
+                "json",
+                "dockerfile",
+                "gitignore",
+                "lua",
+                "yaml",
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+        })
+    end,
 }
